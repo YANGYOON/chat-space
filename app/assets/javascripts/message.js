cb1beb44ee9f032a2c417459cbce2b2386c310e8
YANG LIU <lynk2010@live.jp>
2019-12-12 21:32:34 +0900

    auto-renew

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,46 @@
 $(function(){
 
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id =  $('.message').last().data("message_id")
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+    })
+    .fail(function() {
+      console.log('error');
+    });
+   }
+  };
+
   function buildHTML(message){
     // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
       var html = 
-      `<div class="message">
+      `<div class="message" data-message_id=${message.id}>
           <div class="message__upper-info">
             <div class="message__upper-info__talker">
               ${message.user_name}
             </div>
             <div class="message__upper-info__date">
-              ${message.date}
+              ${message.created_at}
             </div>
           </div>
           <div class="message__text">
@@ -24,13 +54,13 @@ $(function(){
 
     } else {
       var html = 
-      `<div class="message">
+      `<div class="message" data-message_id=${message.id}>
           <div class="message__upper-info">
             <div class="message__upper-info__talker">
               ${message.user_name}
             </div>
             <div class="message__upper-info__date">
-              ${message.date}
+              ${message.created_at}
             </div>
           </div>
           <div class="message__text">
@@ -66,4 +96,5 @@ $(function(){
     });
     return false;
   }); 
+  setInterval(reloadMessages, 7000);
 });
