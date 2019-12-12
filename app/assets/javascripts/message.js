a685e6272b0759ff98fbbf0ef14d38cabe5970a5
YANG LIU <lynk2010@live.jp>
2019-12-12 21:41:11 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -25,7 +25,7 @@ $(function(){
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
    }
   };
